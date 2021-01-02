6b0f5f536b2c7624e2664174aeb5c36a665ad3f8
Boyan Valchev <valchev.boian@gmail.com>
2021-01-03 01:32:55 +0200

    Multiple processing updates

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Press ⇧F10 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from text_processing import process_documents, get_tokenized_sentences_for_documents, get_sentences_score, \
+    get_summary_for_documents
 
 
 def print_hi(name):
@@ -12,5 +14,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    cleaned_documents = process_documents()
+    #print (get_tokenized_sentences_for_documents())
+    #print (get_sentences_score())
+    get_summary_for_documents()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- text_processing.py
+++ text_processing.py
@@ -2,32 +2,88 @@ import re
 import string
 from os import listdir
 from os.path import isfile, join
+import nltk
 from nltk.tokenize import TweetTokenizer
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk import sent_tokenize
+import heapq
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
 
 
-def process_documents():
+def process_documents(should_clean_document=True):
     documents_clean = []
     documents = get_documents()
     for d in documents:
-        cleaned_document = clean_document(d)
-        documents_clean.append(cleaned_document)
+        with open('./data/' + d, 'r', encoding="utf-8-sig") as document_text:
+            text_to_append = document_text.read()
+            if should_clean_document:
+                text_to_append = clean_document(text_to_append)
+            documents_clean.append(text_to_append)
 
     return documents_clean
 
 
-def get_tokens_for_documents():
-    tokens_for_documents = []
-    clean_documents = process_documents()
-    for document in clean_documents:
-        tokens = get_document_tokens(document)
-        filtered_tokens = get_filtered_document_tokens(tokens)
-        normalized_tokens = get_normalized_tokens(filtered_tokens)
-        tokens_for_documents.append(normalized_tokens)
+def get_summary_for_documents():
+    sentence_scores = get_sentences_score()
+    summary_sentences = heapq.nlargest(30, sentence_scores, key=sentence_scores.get)
+    summary = ' '.join(summary_sentences)
+    print(summary)
 
-    return tokens_for_documents
+
+def get_sentences_score():
+    sentence_scores = {}
+    with open('./data/hotel amira istanbul.txt', 'r', encoding="utf-8-sig") as document_text:
+        word_frequencies = get_weightened_word_frequency(get_document_tokens(document_text.read()))
+        document_tokenized_sentences = get_tokenized_sentences_for_documents()
+        for sentence in document_tokenized_sentences:
+            for word in sentence:
+                if word in word_frequencies.keys():
+                    if word not in sentence_scores.keys():
+                        sentence_scores[word] = word_frequencies[word]
+                    else:
+                        sentence_scores[word] += word_frequencies[word]
+    #TO DO: Create a map of sentences and scores here
+
+    return sentence_scores
+
+
+def get_tokenized_sentences_for_documents():
+    document_senteces = []
+    processed_documents = process_documents(True)
+    for document in processed_documents:
+        document_sentences = sent_tokenize(document)
+        for sentence in document_sentences:
+            tokens = get_document_tokens(sentence)
+            #filtered_tokens = get_filtered_document_tokens(tokens)
+            #normalized_tokens = get_normalized_tokens(filtered_tokens)
+            document_senteces.append(tokens)
+
+    return document_senteces
+
+
+def get_word_frequencies(tokens):
+    token_frequencies = {}
+    for token in tokens:
+        if token not in token_frequencies.keys():
+            token_frequencies[token] = 1
+        else:
+            token_frequencies[token] += 1
+
+    return token_frequencies
+
+
+def get_weightened_word_frequency(tokens):
+    word_frequencies = get_word_frequencies(tokens)
+    maximum_frequency = max(word_frequencies.values())
+
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
+
+    return word_frequencies
 
 
 def clean_document(document):
